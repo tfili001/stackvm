97ee92d3587f5a46ffc3e78079e864816526bb2a
tfili001 <tfili001@odu.edu>
2018-06-13 20:51:31 -0400

    Work in Progress

--- main.cpp
+++ main.cpp
@@ -3,25 +3,18 @@
 int main()
 {
 	Interpreter interpreter;
-		
-		// Compiler Section
-		int     frame_index;
+		int     frame_index = 0;
 		bool test;
 		uint8_t byte_stream[] = {04,60,27,05,96,61};
 		size_t  instr_ct   = sizeof(byte_stream)/sizeof(byte_stream[0]);
 		int	var_ct		  = 4;
 		// Execution Section
-		//     currently 13/92 instructions
-		//	       256 total
 		Frame frame(byte_stream,var_ct);
 		//frame.displayByte_Stream();
-
 		interpreter.frameStack.push_back(frame);
-		
-		frame_index   = 0;
 		interpreter.execFrame(frame_index,instr_ct,test=false);
-		
 		frame.displayStack();
+
 		/*
          *  _Add_support_for:_______________
 		 *     Larger numbers, bipush
